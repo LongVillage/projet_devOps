8626c0164cdf3cc5e4812e7fee8aeca06ab3e0b0
Travail <datategy@Mac-Air-datategy.local>
2025-02-18 11:32:21 +0100

    test

--- services/predict-model/terraform/providers.tf
+++ services/predict-model/terraform/providers.tf
@@ -3,8 +3,6 @@
 ###########################################################
 
 terraform {
-  required_version = ">= 1.3.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -19,33 +17,8 @@ terraform {
       version = "~> 2.5"
     }
   }
-  # (Optionnel) backend "s3" { ... } si tu veux stocker tfstate dans S3
 }
 
-############################################
-# Provider AWS
-############################################
 provider "aws" {
   region = var.aws_region
-}
-
-############################################
-# Provider Kubernetes (alias "eks")
-# (OPTIONNEL si on veut manipuler des ressources K8s)
-############################################
-provider "kubernetes" {
-  alias = "eks"
-
-  # On va plus loin dans main.tf pour lier host/token/CA
-  # via data.aws_eks_cluster, data.aws_eks_cluster_auth
-}
-
-############################################
-# Provider Helm (alias "eks_helm")
-############################################
-provider "helm" {
-  alias = "eks_helm"
-
-  # On va également configurer dans main.tf
-  # kubernetes { host/token/CA }
 }
